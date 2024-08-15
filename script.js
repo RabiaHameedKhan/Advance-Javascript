5ba78b441ee33c2aa80e0ce11cb4d156c58c20bd
RabiaHameedKhan <rabiahameedkhan27@gmail.com>
2024-08-15 09:29:42 +0500

    message

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-// CALL BACKS
+// CALL BACKS: passing a function as parameter in aonother function
+
+// function getdata(dataId, getnextdata){
+    
+//     setTimeout(()=>{
+//         console.log("data", dataId);
+//     if(getnextdata){
+//         getnextdata();
+//     }
+//     }, 2000); //2s=2000ms
+// }
+
+//CALL BACK HELL: problem occurs due to Nested call backs, bad way of programming
+
+// getdata(1, ()=>{
+//     console.log("Getting data 2....", );
+//     getdata(2, ()=>{
+//         console.log("Getting data 3....", );
+//         getdata(3, ()=>{
+//             console.log("Getting data 4......");
+//             getdata(4);
+//         });
+//     });
+// });
+
+//PROMISES: To solve the problem of call back hell, eventual completion of task
+
+// let promise= new Promise((resolve, reject)=>{
+//     console.log("Displaying Promise");
+    // (state will be pending)
+    // resolve(123);
+    //(state will be fulfilled)
+    // reject("error");
+    //(state will be rejected)
+// })
+
+
+function getdata(dataId, getnextdata){
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            console.log("data", dataId);
+            resolve("Successful");
+            if(getnextdata){
+                getnextdata();
+            }
+        }, 3000)
+    })
+}
